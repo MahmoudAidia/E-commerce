1f0df343910d792a685e50eec00ed1b0ddd0764d
Mahmoud Mostafa Aidia <116117523+MahmoudAidia@users.noreply.github.com>
2024-05-17 17:14:51 +0300

    Some additional modification to the slider

--- client/src/Components/Slider/Slider.jsx
+++ client/src/Components/Slider/Slider.jsx
@@ -2,11 +2,10 @@ import WestIcon from "@mui/icons-material/West";
 import EastIcon from "@mui/icons-material/East";
 import "./Slider.scss";
 import { useState } from "react";
-
-const images = [
-  "https://images.unsplash.com/photo-1473445195779-499faea82cb7?q=80&w=1818&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1504593811423-6dd665756598?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1444076295597-e246c794dc5f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+const data = [
+  "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
 ];
 
 function Slider() {
@@ -32,9 +31,9 @@ function Slider() {
         className="container"
         style={{ transform: `translate(-${currentSlide * 100}vw)` }}
       >
-        <img src={images[0]} />
-        <img src={images[1]} />
-        <img src={images[2]} />
+        <img src={data[0]} />
+        <img src={data[1]} />
+        <img src={data[2]} />
       </div>
 
       <div className="icons">
